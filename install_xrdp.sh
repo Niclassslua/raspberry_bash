fda5532642e3e14b32b24126e811a7cd2f9a0636
Niclassslua <78554432+Niclassslua@users.noreply.github.com>
2024-10-18 17:10:23 +0200

    Update install_xrdp.sh

--- install_xrdp.sh
+++ install_xrdp.sh
@@ -16,7 +16,11 @@ sudo sed -i.bak '/^test -x \/etc\/X11\/Xsession && exec \/etc\/X11\/Xsession$/s/
 sudo sed -i '/^exec \/bin\/sh \/etc\/X11\/Xsession$/s/^/#/' /etc/xrdp/startwm.sh
 echo "startlxde" | sudo tee -a /etc/xrdp/startwm.sh
 
+# Set keyboard layout override for German QWERTZ in xrdp.ini
+sudo sed -i '/keyboard_layout=/d' /etc/xrdp/xrdp.ini
+echo "xrdp.override_keylayout=0x00000407" | sudo tee -a /etc/xrdp/xrdp.ini
+
 # Restart xrdp service to apply changes
 sudo systemctl restart xrdp
 
-echo "xrdp installation and setup complete. You can now connect to your Raspberry Pi via Remote Desktop."
+echo "xrdp installation and setup complete with German QWERTZ keyboard layout. You can now connect to your Raspberry Pi via Remote Desktop."
