0950c63af51a599e3040766a8fe22da55bd4aa19
Niclassslua <78554432+Niclassslua@users.noreply.github.com>
2024-10-19 19:42:27 +0200

    Update install_xrdp.sh

--- install_xrdp.sh
+++ install_xrdp.sh
@@ -16,11 +16,59 @@ sudo sed -i.bak '/^test -x \/etc\/X11\/Xsession && exec \/etc\/X11\/Xsession$/s/
 sudo sed -i '/^exec \/bin\/sh \/etc\/X11\/Xsession$/s/^/#/' /etc/xrdp/startwm.sh
 echo "startlxde" | sudo tee -a /etc/xrdp/startwm.sh
 
-# Set keyboard layout override for German QWERTZ in xrdp.ini
+# Interactively ask for the new username
+read -p "Enter the name of the new user for xrdp: " NEW_USER
+
+# Create the new user
+sudo adduser $NEW_USER
+
+# Interactively ask for the password
+echo "Set a password for the new user '$NEW_USER':"
+sudo passwd $NEW_USER
+
+# Add the new user to the sudo group
+sudo usermod -aG sudo $NEW_USER
+
+# Interactively choose the keyboard layout for xrdp
+echo "Choose your keyboard layout for xrdp:"
+echo "1) German (QWERTZ)"
+echo "2) US English (QWERTY)"
+echo "3) UK English (QWERTY)"
+echo "4) French (AZERTY)"
+echo "5) Spanish"
+read -p "Enter the number corresponding to your keyboard layout: " LAYOUT_CHOICE
+
+# Map the user's choice to the appropriate layout code
+case $LAYOUT_CHOICE in
+    1)
+        LAYOUT_CODE="0x00000407"
+        ;;
+    2)
+        LAYOUT_CODE="0x00000409"
+        ;;
+    3)
+        LAYOUT_CODE="0x00000809"
+        ;;
+    4)
+        LAYOUT_CODE="0x0000040C"
+        ;;
+    5)
+        LAYOUT_CODE="0x0000040A"
+        ;;
+    *)
+        echo "Invalid choice, defaulting to US English (QWERTY)"
+        LAYOUT_CODE="0x00000409"
+        ;;
+esac
+
+# Set keyboard layout override in xrdp.ini
 sudo sed -i '/keyboard_layout=/d' /etc/xrdp/xrdp.ini
-echo "xrdp.override_keylayout=0x00000407" | sudo tee -a /etc/xrdp/xrdp.ini
+echo "xrdp.override_keylayout=$LAYOUT_CODE" | sudo tee -a /etc/xrdp/xrdp.ini
 
 # Restart xrdp service to apply changes
 sudo systemctl restart xrdp
 
-echo "xrdp installation and setup complete with German QWERTZ keyboard layout. You can now connect to your Raspberry Pi via Remote Desktop."
+# Final messages
+echo "xrdp installation and setup complete with the selected keyboard layout ($LAYOUT_CODE)."
+echo "A new sudo user '$NEW_USER' has been created."
+echo "You can now connect to your Raspberry Pi via Remote Desktop using the '$NEW_USER' account."
